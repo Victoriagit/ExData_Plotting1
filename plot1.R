5fe7ee25bdfc80f45b3ef42528d01c8f8b7405bf
Victoriagit <victoriafarin@icloud.com>
2017-04-24 01:11:30 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+##Loading the dataset
+e_power <- read.csv(file = "/User/cfarin/Downloads/household_power_consumption.txt", 
+        header = TRUE, sep = ";", na.strings = "?")
+
+## Subsetting the Date
+date <- subset(e_power_consumption, subset=(Date == "2007-02-01" & Date == "2007-02-02"))
+
+##Converting the date variable to date class
+date_class <- as.Date(e_power_consumption$Date, format = "%d/%m/%Y")
+
+##Converting the Date and Time
+date_time <- paste(e_power_consumption$Date), e_power_consumption$Time)
+
+##Plotting & Saving to png file 
+
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+hist(e_power_consumption$Global_active_power, main = "Global Active Power", 
+      col = "Red", xlab = "Global Active Power (kilowatts)")
+dev.off()
